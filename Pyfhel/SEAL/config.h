228260b811c299f9caea78ef8c7e1614a163c0fe
Alberto Pimpo <alberto.pimpo@gmail.com>
2020-12-03 12:29:18 +0100

    Snapshot for the exam

---
+++ Pyfhel/SEAL/config.h
@@ -0,0 +1,12 @@
+#define SEAL_VERSION "2.3.1"
+/* #undef SEAL_DEBUG */
+/* #define SEAL_USE_INTRIN */
+#undef _M_X64
+#define SEAL_BUILD_SEAL_C
+#define SEAL_USE___BUILTIN_CLZLL
+#define SEAL_USE___INT128
+#define SEAL_USE__ADDCARRY_U64
+#define SEAL_USE__SUBBORROW_U64
+/* #undef SEAL_USE_MSGSL */
+/* #undef SEAL_USE_MSGSL_SPAN */
+/* #undef SEAL_USE_MSGSL_MULTISPAN */
